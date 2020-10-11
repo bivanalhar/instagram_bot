210e5505bb7425bb289cfa340dd74aa9bf8b8f7b
Bivan Harmanto <bivan.harmanto@shopee.com>
2020-10-11 18:14:17 +0800

    infinite loop, and customized settings for follower counts and account to follow

--- bot_instapy.py
+++ bot_instapy.py
@@ -1,5 +1,19 @@
 from instapy import InstaPy
 import time
+import sys
+
+if len(sys.argv) <= 1:
+    print("need to input all usernames that is to be targeted on")
+    sys.exit(0)
+
+username = sys.argv[1:-1]
+print(username)
+
+try:
+    followCount = int(sys.argv[-1])
+except ValueError:
+    print("the last input of the command line should be a valid integer")
+    sys.exit(0)
 
 session1 = InstaPy(username="maichaelrich001@yahoo.com", password="michael.richimon1")
 session1.login()
@@ -7,11 +21,11 @@ session1.login()
 session2 = InstaPy(username="maichaelrich002@yahoo.com", password="michael.richimon1")
 session2.login()
 
-for i in range(2):
+while True:
     start = time.time()
-    session1.follow_user_followers(['najwashihab'], amount = 15, sleep_delay=1)
+    session1.follow_user_followers(username, amount = 15, sleep_delay=1)
     print("{} seconds".format(time.time() - start))
 
     start = time.time()
-    session2.follow_user_followers(['najwashihab'], amount= 15, sleep_delay=1)
+    session2.follow_user_followers(username, amount= 15, sleep_delay=1)
     print("{} seconds".format(time.time() - start))
